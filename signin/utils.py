76908114810899d54dca0b4ea00b6ca80a76ef39
Xavier Kibet <xavierkibet@gmail.com>
2018-07-08 14:20:50 +0300

    Update utils.py

--- signin/utils.py
+++ signin/utils.py
@@ -1,3 +1,8 @@
 def get_client_ip(request):
-    ip = request.META.get('REMOTE_ADDR')
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        # Take the last entry which is not spoofable
+        ip = x_forwarded_for.split(',')[-1]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
     return ip
